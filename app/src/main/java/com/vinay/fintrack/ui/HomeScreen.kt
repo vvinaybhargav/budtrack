0bb2f042352b6537c5b24fa4b4b46a74fa62afa9
vvinaybhargav <vvinaybhargav@gmail.com>
2026-09-15 09:02:58 +0530

    Add missing LaunchedEffect import in HomeScreen

--- app/src/main/java/com/vinay/fintrack/ui/HomeScreen.kt
+++ app/src/main/java/com/vinay/fintrack/ui/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
